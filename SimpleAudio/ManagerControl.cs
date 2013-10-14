55d3e9c3dbcb0039041d65f37da58e1e4b23f8e7
deaddog <mikkelslsl@gmail.com>
2013-10-14 20:13:09 +0200

    ManagerControl uses fixed size for list and textbox.

--- SimpleAudio/ManagerControl.cs
+++ SimpleAudio/ManagerControl.cs
@@ -3,6 +3,12 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MoonifyControls;
 
+using DeadDog.Audio;
+using DeadDog.Audio.Libraries;
+using DeadDog.Audio.Scan;
+
+using DeadDog.GUI;
+
 namespace SimpleAudio
 {
     public class ManagerControl : XNAControls.Forms.GraphicsDeviceControl
@@ -10,26 +16,26 @@ namespace SimpleAudio
         private ControlManager manager;
 
         private TextBox search;
-        private ListBox<string> list;
+        private ListBox<Track> list;
 
         public ManagerControl()
             : base("Content")
         {
             manager = new MoonifyControls.ControlManager(this.Handle, Services, GraphicsDevice);
-        
+
             search = new MoonifyControls.TextBox()
             {
                 IconType = MoonifyControls.TextBox.IconTypes.Search,
                 BackgroundText = "Search...",
                 Location = new Vector2(10, 19),
-                Width = this.Width - 20
+                Width = 280
             };
             search.TextChanged += (s, e) => search.BackgroundText = search.Text.Length > 0 ? "" : "Search...";
-            list = new ListBox<string>("Hej", "med", "dig", "Metallica")
+            list = new ListBox<Track>()
             {
                 Location = new Vector2(10, 52),
-                Width = this.Width - 20,
-                Height = this.Height - 62
+                Width = 280,
+                Height = 438
             };
 
             manager.Controls.Add(search);
@@ -38,23 +44,10 @@ namespace SimpleAudio
             manager.KeyboardControl = search;
         }
 
-        protected override void OnResize(System.EventArgs e)
-        {
-            base.OnResize(e);
-
-            if (search != null)
-                search.Width = this.Width - 20;
-            if (list != null)
-            {
-                list.Width = this.Width - 20;
-                list.Height = this.Height - 62;
-            }
-        }
-
         protected override void Draw(SpriteBatch spritebatch, GameTime gameTime)
         {
             spritebatch.Begin();
-            spritebatch.Draw(manager.Background,Vector2.Zero, Color.White);
+            spritebatch.Draw(manager.Background, Vector2.Zero, Color.White);
             spritebatch.End();
 
             manager.Draw(gameTime);
