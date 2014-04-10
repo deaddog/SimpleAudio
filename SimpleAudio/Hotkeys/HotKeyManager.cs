10762ffb9944de5a8225cf984120b3ba3cdd0a5e
deaddog <mikkelslsl@gmail.com>
2014-04-10 22:40:24 +0200

    Created Win32 methods for registering/unregistering hotkeys.

--- SimpleAudio/Hotkeys/HotKeyManager.cs
+++ SimpleAudio/Hotkeys/HotKeyManager.cs
@@ -1,6 +1,40 @@
-﻿namespace SimpleAudio.Hotkeys
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace SimpleAudio.Hotkeys
 {
     public class HotKeyManager
     {
+
+        #region HotKey Interop
+
+        private const int WM_HotKey = 786;
+
+        [DllImport("user32", CharSet = CharSet.Ansi,
+                   SetLastError = true, ExactSpelling = true)]
+        private static extern int RegisterHotKey(IntPtr hwnd,
+                int id, int modifiers, int key);
+
+        [DllImport("user32", CharSet = CharSet.Ansi,
+                   SetLastError = true, ExactSpelling = true)]
+        private static extern int UnregisterHotKey(IntPtr hwnd, int id);
+
+        #endregion
+
+        #region Interop-Encapsulation
+
+        private HwndSourceHook hook;
+        private HwndSource hwndSource;
+
+        private void RegisterHotKey(int id, HotKey hotKey)
+        {
+        }
+
+        private void UnregisterHotKey(int id)
+        {
+        }
+
+        #endregion
     }
 }
