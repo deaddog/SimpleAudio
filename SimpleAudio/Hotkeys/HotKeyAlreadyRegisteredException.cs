fd1cbf91582a6f894199c0d06caaf3f0a092bdeb
deaddog <mikkelslsl@gmail.com>
2014-04-10 22:46:37 +0200

    Added an exception class for handling hotkey-duplicates.

---
+++ SimpleAudio/Hotkeys/HotKeyAlreadyRegisteredException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleAudio.Hotkeys
+{
+    public class HotKeyAlreadyRegisteredException : Exception
+    {
+        private HotKey hotkey;
+        public HotKey HotKey
+        {
+            get { return hotkey; }
+        }
+            
+        public HotKeyAlreadyRegisteredException(string message, HotKey hotkey)
+            : base(message)
+        {
+            this.hotkey = hotkey;
+        }
+        public HotKeyAlreadyRegisteredException(string message, HotKey hotkey, Exception inner)
+            : base(message, inner)
+        {
+            this.hotkey = hotkey;
+        }
+    }
+}
