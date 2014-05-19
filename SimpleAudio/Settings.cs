9f70193c4bac70f59190cfe0d34033463a8f136e
stufkan <stufkan@gmail.com>
2014-05-19 20:55:01 +0200

    Class for settings

---
+++ SimpleAudio/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAudio
+{
+    public class Settings
+    {
+        List<String> mediapaths;
+
+        public List<String> Mediapaths{get{return mediapaths;}}
+
+        public Settings()
+        {
+            mediapaths = new List<string>();
+        }
+
+        public void AddMediaPaths(IEnumerable<string> mediapaths)
+        {
+            this.mediapaths.AddRange(mediapaths);
+        }
+    }
+}
