42f2cfe0525470dc3529bce67515153e6b7c4b0f
stufkan <stufkan@gmail.com>
2014-05-19 21:23:30 +0200

    XMLparser for parsing settings

--- SimpleAudio/XMLParser.cs
+++ SimpleAudio/XMLParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,47 @@ namespace SimpleAudio
 {
     public class XMLParser
     {
-        XmlReader reader = XmlReader.Create("settings.xml");
-    
-        
+        ///XML example Format
+        /*
+         <?xml version="1.0" encoding="UTF-8"?>                     
+            <Settings>  
+		        <media>   
+			        <local path="C:\Users\Stefan\Downloads"/>                                                
+			        <local path="C:\Users\Stefan\Music"/>                                       
+		        </media>
+            </Settings>
+         */
+
+        /// <summary>
+        /// Load settings from an xmlfile into the application
+        /// </summary>
+        /// <param name="path">Path to the settings file</param>
+        /// <returns></returns>
+        public Settings LoadSettings(string path)
+        {
+            Settings settings = new Settings();
+            using (FileStream filestream = File.OpenRead(path))
+            {
+                XmlReaderSettings s = new XmlReaderSettings();
+
+                using (XmlReader reader = XmlReader.Create(filestream, s))
+                {
+                    reader.ReadToFollowing("media");
+                    XmlReader inner = reader.ReadSubtree();
+                    settings.AddMediaPaths(ReadMedia(inner));
+                }
+            }
+
+            return settings;
+        }
+        private IEnumerable<string> ReadMedia(XmlReader media)
+        {
+
+            while (media.ReadToFollowing("local"))
+            {
+                media.MoveToFirstAttribute();
+                yield return media.ReadContentAsString();
+            }
+        }
     }
 }
