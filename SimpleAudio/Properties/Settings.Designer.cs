66216213039c64983b580979dabc84280bc21ec3
deaddog <mikkelslsl@gmail.com>
2014-09-18 18:16:36 +0200

    Removed the 'settingsfile' setting from the project-settings collection.

--- SimpleAudio/Properties/Settings.Designer.cs
+++ SimpleAudio/Properties/Settings.Designer.cs
@@ -22,14 +22,5 @@ namespace SimpleAudio.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("settings.xml")]
-        public string settingsfile {
-            get {
-                return ((string)(this["settingsfile"]));
-            }
-        }
     }
 }
