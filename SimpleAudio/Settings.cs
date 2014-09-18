62653b1f98a28d5bb8b9db2d6c8057312f7feac6
deaddog <mikkelslsl@gmail.com>
2014-09-18 17:44:13 +0200

    Settings defines a class for handling media paths.

--- SimpleAudio/Settings.cs
+++ SimpleAudio/Settings.cs
@@ -64,5 +64,64 @@ namespace SimpleAudio
 
             return settings;
         }
+
+        public class MediaPathCollection : IEnumerable<string>
+        {
+            private XElement mediaElement;
+            private List<string> paths;
+
+            public MediaPathCollection(XElement mediaElement)
+            {
+                this.mediaElement = mediaElement;
+                paths = new List<string>(from e in mediaElement.Elements()
+                                         let p = e.Attribute("path")
+                                         where e.Name == "local" && p != null
+                                         select p.Value);
+            }
+
+            public void Add(string path)
+            {
+                if (paths.Contains(path))
+                    throw new ArgumentException("A mediapath can only be added once.", "path");
+
+                paths.Add(path);
+                mediaElement.Add(new XElement("local", new XAttribute("path", path)));
+            }
+            public bool Remove(string path)
+            {
+                if (!paths.Contains(path))
+                    return false;
+
+                paths.Remove(path);
+                var e = getElementFromPath(path);
+
+                if (e != null)
+                    e.Remove();
+
+                return true;
+            }
+
+            private XElement getElementFromPath(string path)
+            {
+                foreach (var e in mediaElement.Elements("local"))
+                {
+                    var a = e.Attribute("path");
+                    if (a != null && a.Value == path)
+                        return e;
+                }
+                return null;
+            }
+
+            IEnumerator<string> IEnumerable<string>.GetEnumerator()
+            {
+                foreach (var p in paths)
+                    yield return p;
+            }
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                foreach (var p in paths)
+                    yield return p;
+            }
+        }
     }
 }
