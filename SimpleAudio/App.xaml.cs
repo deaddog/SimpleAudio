87de32d6f68d9268449f44bc9bac6d8743ab6398
deaddog <mikkelslsl@gmail.com>
2014-09-18 17:09:52 +0200

    Implements methods in the App class that creates a SimpleAudio dir in the users roaming dir and exposes it through the ApplicationDataPath property.

--- SimpleAudio/App.xaml.cs
+++ SimpleAudio/App.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 
 namespace SimpleAudio
@@ -13,5 +9,41 @@ namespace SimpleAudio
     /// </summary>
     public partial class App : Application
     {
+        private static string applicationDataPath;
+        public static string ApplicationDataPath
+        {
+            get { return applicationDataPath; }
+        }
+
+        static App()
+        {
+            var roamingPath = Environment.GetFolderPath(
+                Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create);
+
+            roamingPath = Path.Combine(roamingPath, "DeadDog", "SimpleAudio");
+            ensurePath(roamingPath);
+            applicationDataPath = roamingPath;
+        }
+
+        private static void ensurePath(string path)
+        {
+            string[] levels = path.Split(Path.DirectorySeparatorChar);
+            var drive = new DriveInfo(levels[0]);
+            if (drive.DriveType == DriveType.NoRootDirectory ||
+                drive.DriveType == DriveType.Unknown)
+                throw new ArgumentException("Unable to evaluate path drive; " + levels[0], "path");
+
+            if (!drive.IsReady)
+                throw new ArgumentException("Drive '" + levels[0] + "' is not ready.", "path");
+
+            path = levels[0] + "\\";
+            for (int i = 1; i < levels.Length; i++)
+            {
+                path = Path.Combine(path, levels[i]);
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (!dir.Exists)
+                    dir.Create();
+            }
+        }
     }
 }
