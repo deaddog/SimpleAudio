012cc096d29fcab00c5de03ece51cf935d6fa571
deaddog <mikkelslsl@gmail.com>
2014-10-18 13:20:57 +0200

    Added methods to CoverLoader that will download the response from a http request as a string.

--- SimpleAudio/CoverLoader.cs
+++ SimpleAudio/CoverLoader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,6 +33,61 @@ namespace SimpleAudio
                 "http://www.theaudiodb.com/api/v1/json/{0}/searchalbum.php?s={1}&a={2}";
 
             return string.Format(format, AUDIO_DB_API_KEY, artist, album);
+        }
+
+        private string queryURL(string url)
+        {
+            MemoryStream stream = new MemoryStream();
+            byte[] buf = new byte[8192];
+
+            HttpWebRequest request;
+            HttpWebResponse response = null;
+            Stream resStream = null;
+
+            try
+            {
+                request = (HttpWebRequest)WebRequest.Create(url);
+                response = (HttpWebResponse)request.GetResponse();
+
+                resStream = response.GetResponseStream();
+                copyStream(resStream, stream);
+                var v = response.ContentEncoding;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (resStream != null)
+                {
+                    resStream.Close();
+                    resStream.Dispose();
+                }
+                if (response != null)
+                {
+                    response.Close();
+                    response.Dispose();
+                }
+            }
+
+            string result = Encoding.UTF8.GetString(stream.GetBuffer());
+            stream.Dispose();
+
+            return result;
+        }
+
+        private void copyStream(Stream source, Stream destination)
+        {
+            byte[] buf = new byte[8192];
+            int count = 0;
+            do
+            {
+                count = source.Read(buf, 0, buf.Length);
+                if (count > 0)
+                    destination.Write(buf, 0, count);
+            }
+            while (count > 0);
         }
     }
 }
