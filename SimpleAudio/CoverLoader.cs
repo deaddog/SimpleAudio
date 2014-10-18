40bde184fe75baa5830748bc443656fba1e67e71
deaddog <mikkelslsl@gmail.com>
2014-10-18 13:06:20 +0200

    Created a method for generating album-query url.

--- SimpleAudio/CoverLoader.cs
+++ SimpleAudio/CoverLoader.cs
@@ -8,5 +8,29 @@ namespace SimpleAudio
 {
     public class CoverLoader
     {
+        private const int AUDIO_DB_API_KEY = 1;
+
+        private string getSearchURL(string artist, string album)
+        {
+            if (artist != null)
+            {
+                artist = artist.Trim();
+                if (artist.Length == 0)
+                    artist = null;
+            }
+
+            if (album == null)
+                throw new ArgumentNullException("album");
+
+            if (album != null) album = album.Trim();
+            if (album.Length == 0)
+                throw new ArgumentException("Cannot query an empty album name.");
+
+            string format = artist == null ?
+                "http://www.theaudiodb.com/api/v1/json/{0}/searchalbum.php?a={2}" :
+                "http://www.theaudiodb.com/api/v1/json/{0}/searchalbum.php?s={1}&a={2}";
+
+            return string.Format(format, AUDIO_DB_API_KEY, artist, album);
+        }
     }
 }
