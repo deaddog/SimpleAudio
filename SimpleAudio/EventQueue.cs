0fb2758fe970c6122c1dcb9e46a76b2e1f83a086
deaddog <mikkelslsl@gmail.com>
2014-12-17 16:46:10 +0100

    Created a new EventQueue class that uses events to notify when elements are enqueued/dequeued.

---
+++ SimpleAudio/EventQueue.cs
@@ -0,0 +1,88 @@
+﻿using DeadDog.Audio;
+using DeadDog.Audio.Libraries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAudio
+{
+    public class EventQueue : IQueue<Track>
+    {
+        private Queue<Track> myqueue;
+        public EventQueue()
+        {
+            this.myqueue = new Queue<Track>();
+        }
+
+        public int Count
+        {
+            get { return myqueue.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public event EventHandler<QueuedEventArgs> Enqueued;
+        public event EventHandler<QueuedEventArgs> Dequeued;
+
+        public void Enqueue(Track entry)
+        {
+            myqueue.Enqueue(entry);
+            if (Enqueued != null)
+                Enqueued(this, new QueuedEventArgs(entry));
+        }
+        public Track Dequeue()
+        {
+            var entry = myqueue.Dequeue();
+            if (Dequeued != null)
+                Dequeued(this, new QueuedEventArgs(entry));
+            return entry;
+        }
+
+        public void Clear()
+        {
+            if (Dequeued != null)
+                foreach (var c in myqueue)
+                    Dequeued(this, new QueuedEventArgs(c));
+            myqueue.Clear();
+        }
+
+        public bool Contains(Track item)
+        {
+            return myqueue.Contains(item);
+        }
+
+        public void CopyTo(Track[] array, int arrayIndex)
+        {
+            myqueue.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Track> GetEnumerator()
+        {
+            return myqueue.GetEnumerator();
+        }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return myqueue.GetEnumerator();
+        }
+
+        public class QueuedEventArgs : EventArgs
+        {
+            private Track track;
+
+            public QueuedEventArgs(Track track)
+            {
+                this.track = track;
+            }
+
+            public Track Track
+            {
+                get { return track; }
+            }
+        }
+    }
+}
