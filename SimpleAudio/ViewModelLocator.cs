315c4cb00197817db47c656a5f725d4dd8aab2f5
deaddog <mikkelslsl@gmail.com>
2017-06-19 18:07:19 +0200

    Defined empty view models for searching and for the application status (popup).

---
+++ SimpleAudio/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using SimpleAudio.ViewModels;
+
+namespace SimpleAudio
+{
+    public class ViewModelLocator
+    {
+        public SearchViewModel SearchViewModel
+        {
+            get { return new SearchViewModel(); }
+        }
+
+        public StatusViewModel StatusViewModel
+        {
+            get { return new StatusViewModel(); }
+        }
+    }
+}
