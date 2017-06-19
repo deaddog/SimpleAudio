cc520f43a7aa5163abc5574349a25dcbaeec70c0
deaddog <mikkelslsl@gmail.com>
2017-06-19 19:38:47 +0200

    Added Autofac and used it to instantiate the data context for the views.

--- SimpleAudio/ViewModelLocator.cs
+++ SimpleAudio/ViewModelLocator.cs
@@ -1,17 +1,49 @@
-﻿using SimpleAudio.ViewModels;
+﻿using Autofac;
+using SimpleAudio.ViewModels;
 
 namespace SimpleAudio
 {
     public class ViewModelLocator
     {
-        public SearchViewModel SearchViewModel
+        #region IsDesignMode property hack
+
+        private System.Windows.DependencyObject dummy = new System.Windows.DependencyObject();
+        private bool IsDesignMode => System.ComponentModel.DesignerProperties.GetIsInDesignMode(dummy);
+
+        #endregion
+
+        private static IContainer _container;
+        private static IContainer Container => _container ?? (_container = CreateContainer());
+        private static IContainer CreateContainer()
         {
-            get { return new SearchViewModel(); }
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<StatusViewModel>().SingleInstance();
+            builder.RegisterType<SearchViewModel>().SingleInstance();
+
+            return builder.Build();
         }
 
+        public SearchViewModel SearchViewModel
+        {
+            get { return IsDesignMode ? CreateDesignSearchViewModel() : Container.Resolve<SearchViewModel>(); }
+        }
         public StatusViewModel StatusViewModel
         {
-            get { return new StatusViewModel(); }
+            get { return IsDesignMode ? CreateDesignStatusViewModel() : Container.Resolve<StatusViewModel>(); }
+        }
+
+        private SearchViewModel CreateDesignSearchViewModel()
+        {
+            var vm = new SearchViewModel();
+
+            return vm;
+        }
+        private StatusViewModel CreateDesignStatusViewModel()
+        {
+            var vm = new StatusViewModel();
+
+            return vm;
         }
     }
 }
