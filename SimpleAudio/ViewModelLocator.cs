6be02d77fd5e6f728e47381783b784770f08d6db
deaddog <mikkelslsl@gmail.com>
2017-06-20 21:34:33 +0200

    Defined player setup for the designer.

--- SimpleAudio/ViewModelLocator.cs
+++ SimpleAudio/ViewModelLocator.cs
@@ -1,4 +1,5 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
 using DeadDog.Audio;
 using DeadDog.Audio.Libraries;
 using DeadDog.Audio.Playback;
@@ -51,9 +52,77 @@ namespace SimpleAudio
         }
         private StatusViewModel CreateDesignStatusViewModel()
         {
-            var vm = new StatusViewModel();
+            var library = new Library();
+            var playlist = new LibraryPlaylist(library);
+
+            var track = library.AddTrack(new RawTrack(@"C:\song.mp3", "Atlas, Rise!", "Hardwired... to Self-Destruct", 2, "Metallica", 2016));
+
+            var player = new Player<Track>(playlist, new DesignPlayback());
+            var vm = new StatusViewModel(new PlayerViewModel(player));
+
+            playlist.MoveToEntry(track);
+            player.Play();
 
             return vm;
         }
+
+        #region Designer classes
+
+        private class DesignPlayback : IPlayback<Track>
+        {
+            public PlayerStatus Status => PlayerStatus.Playing;
+
+            public uint Position => (1 * 60 + 14) * 1000;
+            public uint Length => (5 * 60 + 36) * 1000;
+
+            public int LeftVolume { get => 0; set { } }
+            public int RightVolume { get => 0; set { } }
+
+            public event EventHandler StatusChanged;
+            public event PositionChangedEventHandler PositionChanged;
+
+            public bool CanOpen(Track element)
+            {
+                return true;
+            }
+
+            public bool Close()
+            {
+                return true;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool Open(Track element)
+            {
+                return true;
+            }
+
+            public bool Pause()
+            {
+                return true;
+            }
+
+            public bool Play()
+            {
+                StatusChanged?.Invoke(this, EventArgs.Empty);
+                PositionChanged?.Invoke(this, new PositionChangedEventArgs(false));
+                return true;
+            }
+
+            public bool Seek(PlayerSeekOrigin origin, uint offset)
+            {
+                return true;
+            }
+
+            public bool Stop()
+            {
+                return true;
+            }
+        }
+
+        #endregion
     }
 }
