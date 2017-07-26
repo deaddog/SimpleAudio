e064f7fe5475e081638968bec0726e4468669073
deaddog <mikkelslsl@gmail.com>
2017-07-26 22:37:10 +0200

    Implemented cover image drag-n-drop in AlbumCoverImage. Covers can be dragged as urls directly.

--- SimpleAudio/Controls/AlbumCoverImage.xaml.cs
+++ SimpleAudio/Controls/AlbumCoverImage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using DeadDog.Audio.AudioDB;
 using DeadDog.Audio.Libraries;
+using Flurl.Http;
 using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -66,7 +68,6 @@ namespace SimpleAudio.Controls
 
             control.AlbumChanged(album);
         }
-
         private void AlbumChanged(Album album)
         {
             if (album == null)
@@ -92,17 +93,23 @@ namespace SimpleAudio.Controls
                     }
                 });
         }
-
         private void ChangeImage(Uri filepath)
         {
-            if(filepath == null)
+            if (filepath == null)
             {
                 image.Source = null;
                 border.Visibility = Visibility.Collapsed;
             }
             else
             {
-                image.Source = new BitmapImage(filepath);
+                var img = new BitmapImage();
+                img.BeginInit();
+                img.CacheOption = BitmapCacheOption.OnLoad;
+                img.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                img.UriSource = filepath;
+                img.EndInit();
+
+                image.Source = img;
                 border.Visibility = Visibility.Visible;
             }
         }
@@ -111,6 +118,86 @@ namespace SimpleAudio.Controls
         {
             get => GetValue(AlbumProperty) as Album;
             set => SetValue(AlbumProperty, value);
+        }
+        
+        private void HandleDragDrop(object sender, DragEventArgs e)
+        {
+            string file = DownloadDragData(e);
+
+            if (file != null && Album != null && !Album.IsUnknown)
+            {
+                Application.Current.Dispatcher.Invoke(() => ChangeImage(null));
+
+                var filepath = GetCoverFilepath(Album);
+                File.Copy(file, filepath, true);
+
+                Application.Current.Dispatcher.Invoke(() => ChangeImage(new Uri(filepath)));
+            }
+        }
+        private void HandleDragEnter(object sender, DragEventArgs e)
+        {
+            string dragData = MapDragData(e);
+            if (dragData == null || Album == null || Album.IsUnknown)
+                e.Effects = DragDropEffects.None;
+        }
+
+        private string MapDragData(DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(string)))
+                return e.Data.GetData(typeof(string)) as string;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length != 1)
+                    return null;
+
+                string file = files[0];
+                if (!System.IO.File.Exists(file))
+                    return null;
+
+                return file;
+            }
+            else
+                return null;
+        }
+        private string DownloadDragData(DragEventArgs e)
+        {
+            var dragData = MapDragData(e);
+
+            if (dragData == null)
+                return null;
+
+            if (Regex.IsMatch(dragData, @"^[a-zA-Z]:\\"))
+                return dragData;
+            else if (Regex.IsMatch(dragData, @"https?://.*\.(jpg|jpeg|png|gif)", RegexOptions.IgnoreCase))
+            {
+                var tempPath = Path.GetTempFileName();
+                var tempPath2 = Path.GetTempFileName();
+
+                var data = dragData.GetBytesAsync().Result;
+                File.WriteAllBytes(tempPath, data);
+
+                try
+                {
+                    using (var tempImage = System.Drawing.Image.FromFile(tempPath))
+                        tempImage.Save(tempPath2, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                catch
+                {
+                    File.Delete(tempPath2);
+                    return null;
+                }
+                finally
+                {
+                    File.Delete(tempPath);
+                }
+
+                return tempPath2;
+            }
+
+            return null;
         }
     }
 }
