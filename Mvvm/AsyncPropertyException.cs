e39ae20d31b93af78c25018eb864a786d35a1c5a
deaddog <mikkelslsl@gmail.com>
2019-01-26 00:38:09 +0100

    Added Mvvm project.

---
+++ Mvvm/AsyncPropertyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mvvm
+{
+    public class AsyncPropertyException : Exception
+    {
+        public AsyncPropertyException(Exception innerException) : base("Loading the async property failed. See the inner exception for details.", innerException)
+        {
+            if (innerException is null)
+                throw new ArgumentNullException(nameof(innerException));
+        }
+    }
+}
